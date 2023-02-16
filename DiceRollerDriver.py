d672e6faddaf70215b83a1c01515f9c527437e94
BlueJay <bluejayton@gmail.com>
2023-02-16 16:49:33 -0600

    Webpage now displays the amount of dice rolled and the result

--- DiceRollerDriver.py
+++ DiceRollerDriver.py
@@ -8,13 +8,11 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def driver():
-    boost = 10
-    setback = 10
-    ability = 10
-    difficulty = 10
-    proficiency = 10
-    challenge = 10
-    force = 10
+    dice_dict = {"Boost" : 10, "Setback" : 10,
+                 "Ability" : 10, "Difficulty" : 10,
+                 "Proficiency" : 10, "Challenge" : 10,
+                 "Force" : 10}
+    
     counted_results_dict = {"Successes" : 0, "Failures" : 0,
                         "Advantages" : 0, "Threats" : 0,
                         "Triumphs" : 0, "Despairs" : 0,
@@ -25,10 +23,15 @@ def driver():
     #def reset (dice):
 
     #Function Calls
-    RollDice.roll_all_dice (boost, setback, ability, difficulty, proficiency, challenge, force, counted_results_dict)
+    RollDice.roll_all_dice (dice_dict, counted_results_dict)
 
     cancelled_results_dict = CancelandFormatResults.cancel_out_results(counted_results_dict)
-
-    return CancelandFormatResults.display_formatted_results(cancelled_results_dict)
+    display_results = CancelandFormatResults.display_formatted_results(cancelled_results_dict)
+    
+    return flask.render_template (
+        "webpage.html",
+        dice_dict = dice_dict,
+        display_results = display_results
+    )
 
 app.run()
--- RollDice.py
+++ RollDice.py
@@ -1,13 +1,13 @@
 import GetResults
 
-def roll_all_dice (boost, setback, ability, difficulty, proficiency, challenge, force, counted_results_dict):
+def roll_all_dice (dice_dict, counted_results_dict):
     
-    GetResults.calculate_boost_dice(boost, counted_results_dict)
-    GetResults.calculate_setback_dice(setback, counted_results_dict)
-    GetResults.calculate_ability_dice(ability, counted_results_dict)
-    GetResults.calculate_difficulty_dice(difficulty, counted_results_dict)
-    GetResults.calculate_proficiency_dice(proficiency, counted_results_dict)
-    GetResults.calculate_challenge_dice(challenge, counted_results_dict)
-    GetResults.calculate_force_dice(force, counted_results_dict)
+    GetResults.calculate_boost_dice(dice_dict["Boost"], counted_results_dict)
+    GetResults.calculate_setback_dice(dice_dict["Setback"], counted_results_dict)
+    GetResults.calculate_ability_dice(dice_dict["Ability"], counted_results_dict)
+    GetResults.calculate_difficulty_dice(dice_dict["Difficulty"], counted_results_dict)
+    GetResults.calculate_proficiency_dice(dice_dict["Proficiency"], counted_results_dict)
+    GetResults.calculate_challenge_dice(dice_dict["Challenge"], counted_results_dict)
+    GetResults.calculate_force_dice(dice_dict["Force"], counted_results_dict)
     
     return
