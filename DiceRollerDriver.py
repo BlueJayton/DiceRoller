2619aa50128affb9b059b765cd5f32dfce597db4
BlueJay <bluejayton@gmail.com>
2023-02-16 16:27:51 -0600

    First webpage iteration

--- DiceRollerDriver.py
+++ DiceRollerDriver.py
@@ -1,36 +1,34 @@
 
-import random
 import flask
-
 import RollDice
 import CancelandFormatResults
+import GetDiceNums
 
+app = flask.Flask(__name__)
 
-
-
-
-boost = 10
-setback = 3
-ability = 10
-difficulty = 11
-proficiency = 10
-challenge = 7
-force = 10
-counted_results_dict = {"Successes" : 0, "Failures" : 0,
+@app.route('/')
+def driver():
+    boost = 10
+    setback = 10
+    ability = 10
+    difficulty = 10
+    proficiency = 10
+    challenge = 10
+    force = 10
+    counted_results_dict = {"Successes" : 0, "Failures" : 0,
                         "Advantages" : 0, "Threats" : 0,
                         "Triumphs" : 0, "Despairs" : 0,
                         "Lights" : 0, "Darks" : 0}
 
+    #def GetDiceCounts (dicecount):
 
-#def GetDiceCounts (dicecount):
+    #def reset (dice):
 
+    #Function Calls
+    RollDice.roll_all_dice (boost, setback, ability, difficulty, proficiency, challenge, force, counted_results_dict)
 
-#def reset (dice):
+    cancelled_results_dict = CancelandFormatResults.cancel_out_results(counted_results_dict)
 
+    return CancelandFormatResults.display_formatted_results(cancelled_results_dict)
 
-#Function Calls
-RollDice.roll_all_dice (boost, setback, ability, difficulty, proficiency, challenge, force, counted_results_dict)
-
-cancelled_results_dict = CancelandFormatResults.cancel_out_results(counted_results_dict)
-
-print(CancelandFormatResults.display_formatted_results(cancelled_results_dict))
+app.run()
